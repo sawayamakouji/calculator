25cc34e48bbb44260ad14bb350d5420bb80f7e05
sawayamakouji <sawayamakouji@gmail.com>
2024-03-11 19:52:00 +0900

    666

--- netlify/functions/get-trivia.js
+++ netlify/functions/get-trivia.js
@@ -2,6 +2,7 @@
 const axios = require('axios');
 
 exports.handler = async function(event, context) {
+  const allowedOrigin = 'https://sawasawasawayama5555.netlify.app';
   try {
     const number = event.queryStringParameters.number;
     const response = await axios.post(
@@ -23,35 +24,21 @@ exports.handler = async function(event, context) {
     return {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': '*', // 安全性を高めるために、信頼できるドメインに限定することを推奨します。
+        'Access-Control-Allow-Origin': allowedOrigin,
         'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS' // 必要に応じてメソッドを調整してください。
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
       },
-      body: JSON.stringify({ trivia: response.data.choices[0].text })
+      body: JSON.stringify({ trivia: 'トリビアデータ' })
     };
   } catch (error) {
-    // エラーハンドリングを適切に行います。
-    console.error('Error calling OpenAI API:', error);
-    
-    // エラーメッセージの形式をチェックし、適切なエラーメッセージを返す
-    let errorMessage = 'Unknown error';
-    if (error.response) {
-      // OpenAIからのエラーレスポンスがある場合
-      errorMessage = `OpenAI Error: ${error.response.status} - ${error.response.data.error}`;
-    } else if (error.request) {
-      // リクエストは送信されたが、レスポンスを受け取っていない場合
-      errorMessage = 'No response from OpenAI API';
-    } else {
-      // リクエストの設定時に何かが間違っていた場合
-      errorMessage = error.message;
-    }
-
     return {
-      statusCode: error.response ? error.response.status : 500,
+      statusCode: 500,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': allowedOrigin,
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
       },
-      body: JSON.stringify({ error: 'Failed to fetch trivia.' })
+      body: JSON.stringify({ error: error.message })
     };
   }
 };
--- src/App.tsx
+++ src/App.tsx
@@ -71,8 +71,7 @@ const App: React.FC = () => {
 
   const fetchTrivia = async (number: string) => {
     try {
-      const response = await axios.get(`https://sawasawasawayama5555.netlify.app/.netlify/functions/get-trivia
-      `);
+      const response = await axios.get(`https://sawasawasawayama5555.netlify.app/netlify/functions/get-trivia`);
       setTrivia(response.data.trivia);
       setPopupOpen(true);
     } catch (error) {
